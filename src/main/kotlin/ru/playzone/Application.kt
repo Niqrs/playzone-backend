97f3d43f67625d09f6a8e8f7d1a3fcf9e74ef110
Niqrs <niqr.dev@gmail.com>
2022-12-06 07:50:04 +0300

    Another test deployment

--- src/main/kotlin/ru/playzone/Application.kt
+++ src/main/kotlin/ru/playzone/Application.kt
@@ -12,7 +12,7 @@ import ru.playzone.features.login.configureLoginRouting
 import ru.playzone.features.register.configureRegisterRouting
 import ru.playzone.plugins.*
 
-fun main() { //It should work on the server but it is not
+fun main() { //WHY
     val config = HikariConfig("hikari.properties")
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
