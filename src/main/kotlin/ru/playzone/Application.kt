2b7dfeb20106dfbc3593129bb172aa3e2b663bf1
Niqrs <niqr.dev@gmail.com>
2022-12-27 13:39:18 +0300

    HikariConfig update

--- src/main/kotlin/ru/playzone/Application.kt
+++ src/main/kotlin/ru/playzone/Application.kt
@@ -6,14 +6,19 @@ import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.DatabaseConfig
 import ru.playzone.features.games.configureGameRouting
 import ru.playzone.features.login.configureLoginRouting
 import ru.playzone.features.register.configureRegisterRouting
 import ru.playzone.plugins.*
 
 fun main() { //WHY?
-    val config = HikariConfig("hikari.properties")
+    val config = HikariConfig("hikari.properties").apply {
+        addDataSourceProperty("user", System.getenv("PGUSER"))
+        addDataSourceProperty("password", System.getenv("PGPASSWORD"))
+        addDataSourceProperty("portNumber", System.getenv("PGPORT"))
+        addDataSourceProperty("databaseName", System.getenv("PGDATABASE"))
+        addDataSourceProperty("serverName", System.getenv("PGHOST"))
+    }
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
     embeddedServer(Netty, port = System.getenv("PORT").toInt(), module = Application::applicationModule).start(wait = true)
